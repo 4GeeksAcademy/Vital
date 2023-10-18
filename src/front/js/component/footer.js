9297a1ef7ec9a5778caef004a0a61b7c6c58591b
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-10-18 01:50:10 +0000

    large screen layout, no responsive

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,90 +1,95 @@
 import React from "react";
+import Logo from "../../img/logo-vital.png";
 
 export const Footer = () => (
-  <footer className="footer mt-auto text-center">
-    <footer className="text-white text-center text-lg-start bg-vital-gray">
-      <div className="container p-4">
-        <div className="row">
-          <div className="form-outline  form-white mb-4">
-            <input
-              type="text"
-              id="formControlLg"
-              className="form-control form-control-lg col-4"
-            />
-          </div>
+  <footer className="text-white text-center text-lg-start bg-vital-gray">
+    <div className="container pt-4">
+      <div className="row d-flex justify-content-between pt-4">
+        <div className="col-3">
+          <img
+            src={Logo}
+            alt="logo-vital"
+            border="0"
+            style={{ height: "50px" }}
+          />
         </div>
-        <div className="row mt-4">
-          <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
-            <h5 className="text-uppercase mb-4">About Us</h5>
-            <p align="justify">
-              Arepito arose as an idea to provide excellent service and quality
-              food to our customers. Bringing the best of Venezuelan food to the
-              United States.
-            </p>
-            <p align="justify">
-              Our principal goal is to provide the best service and quality food
-              to our customers. We are a family business that is growing and we
-              want to share our culture with you.
-            </p>
-            <div className="mt-4">
-              <a
-                type="button"
-                className="btn btn-floating btn-light btn-lg rounded-circle mx-1"
-              >
-                <i className="fab fa-facebook-f "></i>
-              </a>
-              <a
-                type="button"
-                className="btn btn-floating btn-light btn-lg rounded-circle mx-1"
-              >
-                <i className="fab fa-instagram "></i>
-              </a>
-              <a
-                type="button"
-                className="btn btn-floating btn-light btn-lg rounded-circle mx-1"
-              >
-                <i className="fa-brands fa-x-twitter"></i>
-              </a>
-              <a
-                type="button"
-                className="btn btn-floating btn-light btn-lg rounded-circle mx-1"
-              >
-                <i className="fab fa-google-plus-g "></i>
-              </a>
-            </div>
-          </div>
-          <div className="col-lg-4 col-md-6 mb-4 mb-md-0"></div>
-          <ul className="fa-ul" style={{ marginLeft: "1.65em" }}>
-            <li className="mb-3">
-              <span className="fa-li">
-                <i className="fas fa-home"></i>
-              </span>
-              <span className="ms-2">Naples, 34110, FL</span>
-            </li>
-            <li className="mb-3">
-              <span className="fa-li">
-                <i className="fas fa-envelope"></i>
-              </span>
-              <span className="ms-2">contact@arepitos.com</span>
-            </li>
-            <li className="mb-3">
-              <span className="fa-li">
-                <i className="fas fa-phone"></i>
-              </span>
-              <span className="ms-2">+ 1 239 999 9999</span>
-            </li>
+        <div className="d-flex mx-auto justify-content-end col-9">
+          <input
+            type="email"
+            className="rounded-pill col-6 me-2 ps-5"
+            placeholder="Enter your email to get the latest news..."
+          />
+          <button
+            className="btn btn-vital-orange text-vital-white rounded-pill px-4"
+            type="submit"
+          >
+            Subscribe
+          </button>
+        </div>
+      </div>
+      <hr class="hr text-vital-orange border-3 mt-5" />
+      <div className="d-flex justify-content-between col-">
+        <div>
+          <h5 className="text-uppercase mb-4">Column One</h5>
+          <ul>
+            <li className="mb-2">Twenty One</li>
+            <li className="mb-2">Twenty Two</li>
+            <li className="mb-2">Twenty Three</li>
+            <li className="mb-2">Twenty Four</li>
           </ul>
         </div>
+        <div>
+          <h5 className="text-uppercase mb-4">Column One</h5>
+          <ul>
+            <li className="mb-2">Twenty One</li>
+            <li className="mb-2">Twenty Two</li>
+            <li className="mb-2">Twenty Three</li>
+            <li className="mb-2">Twenty Four</li>
+          </ul>
+        </div>
+        <div>
+          <h5 className="text-uppercase mb-4">Column One</h5>
+          <ul>
+            <li className="mb-2">Twenty One</li>
+            <li className="mb-2">Twenty Two</li>
+            <li className="mb-2">Twenty Three</li>
+            <li className="mb-2">Twenty Four</li>
+          </ul>
+        </div>
+        <div className="mt-4 d-flex">
+          <a
+            type="button"
+            className="btn btn-floating text-vital-white  btn-lg rounded-circle mx-1"
+          >
+            <i className="fab fa-facebook-f "></i>
+          </a>
+          <a
+            type="button"
+            className="btn btn-floating text-vital-white  btn-lg rounded-circle mx-1"
+          >
+            <i className="fab fa-instagram "></i>
+          </a>
+          <a
+            type="button"
+            className="btn btn-floating text-vital-white  btn-lg rounded-circle mx-1"
+          >
+            <i className="fa-brands fa-x-twitter"></i>
+          </a>
+          <a
+            type="button"
+            className="btn btn-floating text-vital-white  btn-lg rounded-circle mx-1"
+          >
+            <i className="fab fa-google-plus-g "></i>
+          </a>
+        </div>
       </div>
-      <div
-        className="text-center p-3"
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-      >
-        © 2023 Copyright:
-        <a className="text-white" href="#">
-          Vital.com
-        </a>
-      </div>
-    </footer>
+      <hr class="hr text-vital-orange border-3 mt-5" />
+    </div>
+    <div className="text-center p-3">
+      © 2023 Copyright:{" "}
+      <a className="text-white" href="#">
+        Vital.com
+      </a>
+    </div>
   </footer>
 );
