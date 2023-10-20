f4ff90346051fef3ea268d830380281bbe102c63
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-10-20 14:17:56 +0000

    body part view done

--- src/front/js/pages/bodypartExercises.js
+++ src/front/js/pages/bodypartExercises.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "../../styles/bodypart-exercises.css";
 import { useFetch } from "../hooks/useFetch";
 import imageBackgroundArm from "../../img/low-arm.png";
-
+import ExerciseCard from "../component/exerciseCard";
 
 export const BodypartExercises = () => {
   const bodyPart = "chest";
@@ -18,15 +18,19 @@ export const BodypartExercises = () => {
   // const { data, error, loading } = useFetch(url, options);
   // console.log(data);
 
+  const exercises = ["exercise1", "exercise2", "exercise3", "exercise4", "exercise5", "exercise6", "exercise7", "exercise8"];
+
   return (
     <>
       <div
-        className="container-fluid image-background vh-50"
+        className="container-fluid image-part-background"
         style={{ backgroundImage: `url(${imageBackgroundArm})` }}
       >
-        <div className="container w-50 d-flex justify-content-center align-items-center wrap-background-image">
+        <div className="container w-50 h-100 d-flex justify-content-center align-items-center">
           <div className="title">
-            <h1 className="text-vital-orange text-center fw-bold">Routines</h1>
+            <h1 className="text-vital-orange text-center fw-bold">
+              Lower arms
+            </h1>
             <p className="text-vital-white text-center pb-5">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
               accumsan odio ut mi aliquam volutpat. Nam tincidunt quam vitae
@@ -35,17 +39,20 @@ export const BodypartExercises = () => {
           </div>
         </div>
       </div>
-      <div className="container-fluid bg-vital-black">
-        <div className="container d-flex pb-5 flex-column title-workout">
-          <h1 className="text-vital-orange pt-4 text-center fw-bold">
-            Lower arms
-          </h1>
+      <div className="container-fluid p-5 bg-vital-black">
+        <div className="container d-flex  flex-column title-workout">
           <div className="row col-11 d-flex mx-auto justify-content-around">
-            Exercises
+            {
+              exercises.map((exercise, index) => {
+                return (
+                  <ExerciseCard key={index} />
+                );
+              })
+            }
+            
           </div>
         </div>
       </div>
     </>
   );
 };
-
