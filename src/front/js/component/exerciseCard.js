e2bdf322f40f8101be1bd2e15f00721ed1dabc74
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-10-20 18:21:10 +0000

    bold text

--- src/front/js/component/exerciseCard.js
+++ src/front/js/component/exerciseCard.js
@@ -8,7 +8,7 @@ const ExerciseCard = ({ exercises }) => {
       className="wrap-exercise-card  col-3 p-0 pb-2 bg-vital-gray mt-4 border border-1 rounded-3 border-vital-orange"
       // style={{ height: "220px" }}
     >
-      <i class="fa-regular fa-heart" style={{color: "#ff5300"}}></i>
+      <i class="fa-regular fa-heart" style={{ color: "#ff5300" }}></i>
       <div
         className="rounded-3 image-card-exercise d-flex justify-content-center align-items-center"
         style={{
@@ -20,7 +20,7 @@ const ExerciseCard = ({ exercises }) => {
         </h1>
       </div>
       <div className="d-flex-inline flex-column justify-content-start p-2 overflow-y-hidden">
-        <span className="font-p text-vital-orange ">
+        <span className="font-p text-vital-orange fw-bold">
           Nombre de ejercicio, quizás sea largo o corto, no lo sé
         </span>
         <br />
