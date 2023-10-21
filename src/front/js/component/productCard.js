e699c9361b9883f2baf35408cc4d2def91fa00f8
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-10-21 02:03:46 +0000

    Refactorización de ProductCard y mapeo en vista Store

--- src/front/js/component/productCard.js
+++ src/front/js/component/productCard.js
@@ -2,16 +2,16 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import Image from "../../img/image1.png";
 
-const ProductCard = ({ name, price, id }) => {
+const ProductCard = ({ name, price, url, id }) => {
     
     return (
-        <div className="card bg-vital-gray" style={{ width: "18rem" }}>
-        <img src="https://www.valentiamedical.com/795-home_default/rodillera-deportiva-elastica.jpg" className="card-img-top" alt="Imagen del producto" />
+        <div className="card bg-vital-gray m-1" style={{ width: "18rem" }}>
+        <img src={url} className="card-img-top" alt="Imagen del producto" />
         <div className="card-body">
           <h5 className="card-title fw-bold text-vital-orange">{name}</h5>
           <p className="card-text text-vital-white">{price}</p>
           <a href="#" className="btn btn-vital-orange rounded-3 text-vital-white fw-bold">
-            Agregar al carrito
+            Add to cart
           </a>
         </div>
       </div>
--- src/front/js/pages/store.js
+++ src/front/js/pages/store.js
@@ -1,22 +1,27 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import Image from "../../img/image1.png";
-import ProductCard from "../component/productCard"
+import ProductCard from "../component/productCard";
 import { products } from "../constants/constants";
 
 export const Store = () => {
-    
-    return (
-      <div className="container-fluid">
-        {products.map((product, index) => {
-          return (
-            <ProductCard 
-            key={product.id}
-            name={product.name}
-            price={product.price}
-            />
-          )
-        })}
+  return (
+    <div className="container-fluid p-5 bg-vital-black">
+      <div className="container d-flex  flex-column title-workout">
+        <div className="row col-11 d-flex mx-auto justify-content-around">
+          {products.map((product, id) => {
+            return (
+              <ProductCard
+                key={product.id}
+                url={product.url}
+                name={product.name}
+                price={product.price}
+                id={product.id}
+              />
+            );
+          })}
+        </div>
       </div>
-    )
-}
+    </div>
+  );
+};
