890c9d21c23e324def4a6594262bc9a3fcd81b68
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-10-23 18:45:09 +0000

    Conexión de vista Store con Fake Store API. ProductCards se rompen

--- src/front/js/pages/store.js
+++ src/front/js/pages/store.js
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import Image from "../../img/image1.png";
 import ProductCard from "../component/productCard";
-import { products } from "../constants/constants";
+// import { products } from "../constants/constants";
 import BackgroundContainer from "../component/backgroundContainer";
 import Imagen from "../../img/store-background.png"
 
 export const Store = () => {
+
+  const { store, actions } = useContext(Context)
+  const products = store.products
+
   return (
     <>
     <BackgroundContainer 
@@ -16,12 +19,13 @@ export const Store = () => {
       <div className="container-fluid p-5 bg-vital-black">
         <div className="container d-flex  flex-column title-workout">
           <div className="row col-11 d-flex mx-auto justify-content-around">
+
             {products.map((product, id) => {
               return (
                 <ProductCard
                   key={product.id}
-                  url={product.url}
-                  name={product.name}
+                  url={product.image}
+                  name={product.title}
                   price={product.price}
                   id={product.id}
                 />
