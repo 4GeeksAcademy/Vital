5847ad2ea4c7fc87967b88d5b28f04dd91037afd
Stefanolr04 <136249681+Stefanolr04@users.noreply.github.com>
2023-10-24 16:16:32 +0000

    cambio en el event

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -3,11 +3,8 @@ import "../../styles/login.css"
 import { Link } from "react-router-dom";
 
 export const Login = () => {
-    const [login,setLogin]= useState({
-        username: "",
-        password: ""
-
-    })
+    const [email,setEmail]= useState("")
+    const [password,setPassword]= useState("")
     return (
         <div className="bg-vital-black p-5">
             <div className="container bg-vital-gray rounded-3">
@@ -16,19 +13,19 @@ export const Login = () => {
                     <div className="col-6 d-flex-column">
                         <h1 className="d-flex justify-content-center my-4 text-vital-orange">Hello welcome back!</h1>
                         <div className="mb-3">
-                            <label for="exampleFormControlInput1" className="form-label text-vital-white">Username</label>
-                            <input type="string" value={login.username} className="form-control" id="exampleFormControlInput1" onChange={(event)=>{setLogin({...login, username: event.target.value})}}/>
+                            <label for="exampleFormControlInput1" className="form-label text-vital-white">Email</label>
+                            <input type="string" value={email} className="form-control" id="exampleFormControlInput1" onChange={(event)=>{setEmail( event.target.value)}}/>
                         </div>
                         <div className="mb-3">
                             <label for="exampleFormControlInput1" className="form-label text-vital-white">Password</label>
-                            <input type="string" value={login.password} className="form-control" id="exampleFormControlInput1" onChange={(event)=>{setLogin({...login, password: event.target.value})}}/>
+                            <input type="string" value={password} className="form-control" id="exampleFormControlInput1" onChange={(event)=>{setPassword(event.target.value)}}/>
                         </div>
-                        <div className="mb-3">
+                        <div className="mb-3 mt-4">
                             <button className="btn btn-vital-orange text-vital-white w-100">Login</button>
                         </div>
                         <div className="d-flex-column justify-content-center mb-4">
 
-                            <span className="d-flex justify-content-center">Don't have an account?</span>
+                            <span className="d-flex justify-content-center text-vital-white">Don't have an account?</span>
                             <Link to='/register' className="d-flex justify-content-center text-vital-orange text-decoration-none">Create one here</Link>
                             
                         </div>
