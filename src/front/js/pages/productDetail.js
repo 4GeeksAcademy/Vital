968ea52373b9e243e3449bf56e49d85d323a92d1
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-10-24 00:44:03 +0000

    Adaptación de vista ProductDetail a responsive

--- src/front/js/pages/productDetail.js
+++ src/front/js/pages/productDetail.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useParams } from "react-router-dom"
 import gif from "../../img/exercise.gif";
 
-
 const ProductDetail = () => {
 
     const params = useParams()
@@ -10,14 +9,14 @@ const ProductDetail = () => {
     return (
         <div className="container-fluid bg-vital-black d-flex flex-column p-5"> 
             <div className="row">
-                <div className="col-6 text-vital-white">
+                <div className="col-lg-6 col-md-12 text-vital-white">
                     <img
                         className="img-fluid rounded-3"
                         src={gif}
                         alt="Product"
                         />
                     </div>
-                <div className="col-6">
+                <div className="col-lg-6 col-md-12">
                     <h2 className="text-vital-orange fw-bold">
                         Product name
                     </h2>
@@ -31,10 +30,10 @@ const ProductDetail = () => {
                     <span className="text-vital-orange fs-3">$100.00</span>
                     <hr className="text-vital-white"/>
                     <div className="mt-1 row d-flex flex-row">
-                        <div className="col-6">
+                        <div className="col-sm-6 col-12 mb-3 mb-sm-0">
                             <input type="number" className="form-control" placeholder="Qty" />
                         </div>
-                        <div className="col-6">
+                        <div className="col-sm-6 col-12">
                             <button type="button" class="btn btn-vital-orange text-vital-white rounded-5 px-4">Add to cart</button> 
                         </div>
                     </div>
