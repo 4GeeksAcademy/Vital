c0cca23378b2be53240583da7dd24bb756771bd9
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-10-24 03:17:51 +0000

    Botón de regreso de vista ProductDetail a Store

--- src/front/js/component/productCard.js
+++ src/front/js/component/productCard.js
@@ -11,6 +11,7 @@ const ProductCard = ({ name, price, url, id }) => {
         <img src={url} className="card-img-top" alt="Imagen del producto" />
         <div className="card-body">
           <h5 className="card-title fw-bold text-vital-orange"
+          style={{ cursor: "pointer" }}
           onClick={()=> navigate(`/product-detail/${id}`)}>{name}</h5>
           <p className="card-text text-vital-white">{price}</p>
           <a href="#" className="btn btn-vital-orange rounded-3 text-vital-white fw-bold">
