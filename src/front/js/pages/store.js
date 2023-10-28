81d0a768f83ba94b1ae1a5352a9261fc2b60b053
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-10-28 19:54:03 +0000

    getting document and filtering by ID to show it in details product view

--- src/front/js/pages/store.js
+++ src/front/js/pages/store.js
@@ -16,6 +16,7 @@ export const Store = () => {
     // actions.getProducts();
 
     const productsRef = collection(db, "products")
+    console.log(productsRef);
 
     getDocs(productsRef)
       .then((resp) => {
