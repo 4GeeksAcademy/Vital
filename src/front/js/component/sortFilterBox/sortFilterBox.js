6693d65a8f09f8203b31cc3b9a11e6ea6eaf6a84
diegoguillen70 <137721556+diegoguillen70@users.noreply.github.com>
2023-10-29 02:29:12 +0000

    states on sort, filter, search, pagination are set up

--- src/front/js/component/sortFilterBox/sortFilterBox.js
+++ src/front/js/component/sortFilterBox/sortFilterBox.js
@@ -1,56 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sortFilterBox.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
-const SortFilterBox = ({
-  sort,
-  setSort,
-  filter,
-  setFilter,
-  search,
-  setSearch,
-}) => {
+const SortFilterBox = ({ setSearch, setSort, setFilter }) => {
+  const [inputFilter, setInputFilter] = useState("");
+  const [inputSearch, setInputSearch] = useState("");
+
+  const handleSelect = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    setSort(e.target.value);
+  };
+  const handleFilter = (e) => {
+    e.preventDefault();
+    console.log(inputFilter);
+    setFilter(inputFilter);
+  };
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log(inputSearch);
+    setSearch(inputSearch);
+  };
+
   return (
     <div className="container-fluid bg-vital-black">
       <div className="container pt-5 bg-vital-black d-flex justify-content-around">
         <div className="search-container">
           <input
             type="text"
+            value={inputSearch}
             placeholder="Search"
             className="search-input rounded-pill px-3 mx-3"
             style={{ height: "45px" }}
+            onChange={(e) => setInputSearch(e.target.value)}
           />
           <input
-            type="submit"
+            type="button"
             value="Search"
             className="search-button btn btn-vital-orange text-vital-white rounded-pill mx-3"
+            onClick={handleSearch}
           />
         </div>
         <div className="sort-filter-container d-flex">
           <select
             className="form-select form-select-sm rounded-pill px-3 mx-3"
-            aria-label=".form-select-sm example" style={{ height: "45px", width: "80px" }}
+            aria-label=".form-select-sm example"
+            style={{ height: "45px", width: "80px" }}
+            onChange={handleSelect}
           >
             <option defaultValue="">Sort</option>
-            <option value="1">Ascendent </option>
-            <option value="2">Descendent </option>
-            
+            <option value="asc">Ascendent </option>
+            <option value="desc">Descendent </option>
           </select>
-          {/* <input
-            type="select"
-            placeholder="Sort"
-            className="sort-input rounded-pill px-3 mx-3"
-            style={{ height: "45px" }}
-  />*/}
+
           <input
             type="filter"
+            value={inputFilter}
             placeholder="Filter"
             className="filter-input rounded-pill px-3 mx-3"
             style={{ height: "45px" }}
+            onChange={(e) => setInputFilter(e.target.value)}
           />
           <input
-            type="submit"
+            type="button"
             value="Filter"
             className="filter-button btn btn-vital-orange text-vital-white rounded-pill mx-3"
+            onClick={handleFilter}
           />
         </div>
       </div>
--- src/front/js/pages/bodypartExercises.js
+++ src/front/js/pages/bodypartExercises.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import "../../styles/bodypart-exercises.css";
 import imageBackgroundArm from "../../img/low-arm.png";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import ExerciseCard from "../component/exerciseCard";
 import { scrollToTop } from "../function/scrollToTop";
 import BackgroundContainer from "../component/backgroundContainer";
-import { useAPI } from "../constants/constants";
 import { useFetch } from "../hooks/useFetch";
 import { allExercises } from "../constants/allExcercises";
 import { description, dataExcersises } from "../constants/constants";
@@ -13,14 +12,39 @@ import Pagination from "../component/pagination/pagination";
 import SortFilterBox from "../component/sortFilterBox/sortFilterBox";
 import Loading from "../component/loading/loading";
 
+
 export const BodypartExercises = () => {
+  const [loading, setLoading] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [sort, setSort] = useState("ascending");
+  const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("");
+  const [exercises, setExercises] = useState(null)
   const [page, setPage] = useState(1);
+  // const [searchParams] = useSearchParams();
+  const currentPage = searchParams.get("page") ? searchParams.get("page") : 1;
+  const navigate = useNavigate();
   const { bodypart } = useParams();
   const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodypart}?limit=150`; //
 
   useEffect(() => {
     scrollToTop();
+    setPage(currentPage);
+    
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      setExercises(data);
+      setLoading(false);
+    }
+  }, [data]);
+
+  useEffect(() => {    
+    const currentParams = Object.fromEntries([...searchParams]);
+    console.log('Updated:', currentParams);
+    setSearchParams({ sort: sort, page: page, filter: filter, search: search });
+  }, [sort, page, filter, search]);
 
   const options = {
     method: "GET",
@@ -29,6 +53,7 @@ export const BodypartExercises = () => {
       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
     },
   };
+
   const start = (page - 1) * 8;
   const end = page * 8;
   //uncoment the line 35-36 to fetch API and comment the line 39-44
@@ -39,11 +64,12 @@ export const BodypartExercises = () => {
   const dataFilter = allExercises.filter((exercise) => {
     return exercise.bodyPart === bodypart;
   });
-  console.log(dataFilter);
-  const data = dataFilter;  
-  const loading = false;
-
+  //console.log(dataFilter);
+  const data = dataFilter;    
+  
   const title = bodypart.charAt(0).toUpperCase() + bodypart.slice(1);
+  const params = Object.fromEntries([...searchParams]);
+  console.log('Mounted:', params);
 
   return (
     <>
@@ -56,21 +82,22 @@ export const BodypartExercises = () => {
             description={description}
             image={imageBackgroundArm}
           />
-          <SortFilterBox />
+          <SortFilterBox setSort={setSort} setFilter={setFilter} setSearch={setSearch}/>
           <div className="container-fluid p-5 bg-vital-black">
             <div className="container d-flex  flex-column title-workout">
               <div className="row col-11 d-flex mx-auto justify-content-around">
-                {data &&
-                  data.slice(start, end).map((exercise, index) => {
+                {exercises &&
+                  exercises.slice(start, end).map((exercise, index) => {
                     return (
                       <>
-                        {data && (
+                        {exercises && (
                           <ExerciseCard
                             key={index}
                             exercise={exercise.name}
                             id={exercise.id}
                             target={exercise.target}
                             equipment={exercise.equipment}
+                            
                           />
                         )}
                       </>
@@ -80,7 +107,7 @@ export const BodypartExercises = () => {
             </div>
           </div>
           <div>
-            <Pagination exercisesPerPage="8" totalExercises={dataFilter ? dataFilter.length : data.length} setPage={setPage}/>
+            <Pagination exercisesPerPage="8" totalExercises={dataFilter ? dataFilter.length : data.length} setPage={setPage} currentPage={currentPage} searchParams={searchParams} setSearchParams={setSearchParams}/>
           </div>
         </>
       )}
