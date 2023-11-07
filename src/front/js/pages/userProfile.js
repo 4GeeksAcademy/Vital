0bfaba20eac2f1107ce555e6e342b11f67b94cfd
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-11-07 15:22:07 +0000

    :lipstick::construction: Arreglo de estilos

--- src/front/js/pages/userProfile.js
+++ src/front/js/pages/userProfile.js
@@ -4,7 +4,7 @@ import "../../../front/styles/user-profile.css"
 export const UserProfile = () => {
   return (
     <div className='container bg-vital-gray'>
-      <div className='card border border-vital-orange bg-vital-black w-75 my-2'>
+      <div className='card bg-vital-black w-75 my-2'>
         <div className='card-body'>
           <div className='d-flex flex-row'>
             <div className=''>
@@ -15,7 +15,7 @@ export const UserProfile = () => {
                 <h2 className='fs-5 fw-bold text-vital-orange'>Carlos Corona</h2>
                 <p className='text-vital-white'>Soy un disenador grafico estudiando programacion. Me apasiona la tecnologia y la gastronomia. Me gusta mucho viajar.</p>
               </div>
-              <div className='icon-container'>
+              <div className='icon-container ps-1'>
                 <i className="fa-regular fa-pen-to-square text-vital-orange fs-5" ></i>
               </div>
             </div>
