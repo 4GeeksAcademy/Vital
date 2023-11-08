31d01906b2fc2758fe3c951a40037a6216433829
Stefanolr04 <136249681+Stefanolr04@users.noreply.github.com>
2023-11-08 13:24:54 +0000

    adding mealplans design

--- src/front/js/pages/mealPlans.js
+++ src/front/js/pages/mealPlans.js
@@ -1,6 +1,9 @@
 import React, {useContext, useEffect} from "react";
 import { Context } from "../store/appContext";
 import { chooseMeal } from "../constants/constants";
+import BackgroundContainer from "../component/backgroundContainer";
+import Imagen from  "../../img/background-nutrition.png"
+import "../../styles/meals.css"
 
 export const MealPlans = () => {
   const {store, actions} = useContext(Context)
@@ -8,37 +11,64 @@ export const MealPlans = () => {
     actions.getMeals(chooseMeal.lowFat.dinner)
   },[])
     return(
+      <div className="bg-vital-gray">
       
-        <div className="container bg-success p-4 ">
-          <div className="row">
-            <div className="col-12">
-              <div className="input-group mb-3">
-                <input type="text" className="form-control rounded-pill" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-              </div>
-            </div>
-          </div>
-
-          <h1 className="text-vital-orange">Breakfast</h1>
-
-          { store.meals && 
-            store.meals.map((meal, index) => {
-              
-              return ( 
-                <div className="row">
-                  <div className="col">
-                    <div className="card" style={{width: "18rem"}}>
-                      <img src={meal.recipe.image} className="card-img-top" alt="..."/>
-                      <div className="card-body">
-                        <h5 className="card-title">{meal.recipe.label}</h5>
-                        <p className="card-text">{meal.recipe.calories}</p>
-                      </div>
-                    </div>
-                  </div>  
-                </div>
-              )
-            })
-          }
-        </div>
+      <BackgroundContainer
+      title="Choose your Plan"
+      image={Imagen}
       
+    />
+
+    <div className="container-fluid contenido mt-4 ">
+      <div className="carrousel">
+        <article className="card">
+          <h1 className="text-vital-orange">High Fiber</h1>
+
+        </article>
+        <article className="card">
+          <h1 className="text-vital-orange">High Protein</h1>
+        </article>
+        <article className="card">
+          <h1 className="text-vital-orange">Low Carb</h1>
+        </article>
+        <article className="card">
+          <h1 className="text-vital-orange">Low Fat</h1>
+        </article>
+      </div>
+    </div>
+     
+    
+      </div>
+
     )
 }
+    //  <div className="container bg-success p-4 "> 
+    //    <div className="row">
+    //      <div className="col-12">
+    //        <div className="input-group mb-3">
+    //          <input type="text" className="form-control rounded-pill" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+    //        </div>
+     //     </div>
+    //    </div>
+
+    //    <h1 className="text-vital-orange">Breakfast</h1>
+
+    //    { store.meals && 
+    //      store.meals.map((meal, index) => {
+            
+    //        return ( 
+    //          <div className="row">
+     //           <div className="col">
+     //             <div className="card" style={{width: "18rem"}}>
+       //             <img src={meal.recipe.image} className="card-img-top" alt="..."/>
+         //           <div className="card-body">
+           //           <h5 className="card-title">{meal.recipe.label}</h5>
+             //         <p className="card-text">{meal.recipe.calories}</p>
+               //     </div>
+                 // </div>
+           //     </div>  
+           //   </div>
+          //  )
+        //  })
+      //  }
+   //   </div>
