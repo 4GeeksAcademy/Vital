9405840288127a721bcfec08b451c6bcf3fc4e02
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-11-14 01:05:32 +0000

    Mapeo de los ejercicios de store.favorites en vista Favorites

--- src/front/js/component/favoritesCards/favoriteWorkout.js
+++ src/front/js/component/favoritesCards/favoriteWorkout.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Context } from "../../store/appContext";
 
-export const FavoriteWorkout = () => {
+export const FavoriteWorkout = ({exercises, category}) => {
+    const { store } = useContext(Context)
+    const { favorites } = store
+    const navigate = useNavigate()
+
     return (
         <div className='card bg-dark d-flex justify-content-evenly mb-1 p-3'>
             <div>
-                <span className='text-vital-white mb-1'>CATEGORIA</span>
-                <h5 className='text-vital-orange fs-5'>Nombre ejercicio/dieta</h5>
+                <span className='text-vital-white mb-1'>{category}</span>
+                <h5 className='text-vital-orange fs-5'>{exercises}</h5>
             </div>
             <div>
                 <i className="fa-regular fa-trash-can text-vital-white"></i>
--- src/front/js/pages/favorites.js
+++ src/front/js/pages/favorites.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from "../store/appContext"
 import { FavoriteWorkout } from '../component/favoritesCards/favoriteWorkout'
 
 export const Favorites = () => {
+
+    const { store } = useContext(Context)
+    const { favorites } = store
+    const isEmpty = Object.values(favorites).every(bodyPartKey => bodyPartKey.length === 0)
+
+
     return (
         <>
             <h1 className="text-vital-orange fw-bold d-flex justify-content-center mt-2">Favorites</h1>
@@ -9,23 +16,32 @@ export const Favorites = () => {
                 <div className='col-4 my-2 mx-1'>
                     <div className='card container bg-vital-black d-flex'>
                         <h5 className="card-header text-vital-orange fw-bold">Favorite workouts</h5>
-                        <div className='card-body'>
-                            <FavoriteWorkout />
-                            <FavoriteWorkout />
-                            <FavoriteWorkout />
-                        </div>
+                        {
+                            isEmpty ?
+                            <div className='card-body'>
+                                <p className="text-vital-white">You haven't added exercises.</p>
+                            </div>
+                            :
+                            <div className='card-body'>
+                                {
+                                    Object.keys(store.favorites).map((category, index) =>(
+                                        store.favorites[category].map((exercise) => {
+                                            return <FavoriteWorkout exercises={exercise} category={category.toUpperCase()}/>
+                                        })
+                                    ))
+                                }
+                            </div>
+                        }
                     </div>
                 </div>
-                <div className='col-4 my-2'>
+                {/* <div className='col-4 my-2'>
                     <div className='card container bg-vital-black d-flex'>
                         <h5 className="card-header text-vital-orange fw-bold">Favorite meal plans</h5>
                         <div className='card-body'>
                             <FavoriteWorkout />
-                            <FavoriteWorkout />
-                            <FavoriteWorkout />
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
         </>
     )
